b9fb746970543ffef6fdf57f27fe3c2a6cc41c1f
Andre <andrehristu@gmail.com>
2023-03-17 14:24:32 -0400

    added firstname lastname to user model, migrations, and routes

--- authenticate-me/backend/db/seeders/20230317165021-demo-user.js
+++ authenticate-me/backend/db/seeders/20230317165021-demo-user.js
@@ -14,16 +14,22 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         email: 'demo@user.io',
+        firstName: 'firstname1',
+        lastName: 'lastname1',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
+        firstName: 'firstname2',
+        lastName: 'lastname2',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
+        firstName: 'firstname3',
+        lastName: 'lastname3',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
       }
