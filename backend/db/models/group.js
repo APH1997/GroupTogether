276f3bf042ffdaf04abc237a5b8818004e86d878
Andre <andrehristu@gmail.com>
2023-03-18 14:42:47 -0400

    groupimages table complete

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -18,6 +18,9 @@ module.exports = (sequelize, DataTypes) => {
         through: 'Membership',
         foreignKey: 'groupId'
       })
+      Group.hasMany(models.GroupImage, {
+        foreignKey: 'groupId'
+      })
     }
   }
   Group.init({
---
+++ backend/db/models/groupimage.js
@@ -0,0 +1,39 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class GroupImage extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      GroupImage.belongsTo(models.Group, {
+        foreignKey: 'groupId'
+      })
+    }
+  }
+  GroupImage.init({
+    groupId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Groups'
+      }
+    },
+    url: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    preview: {
+      allowNull: false,
+      type: DataTypes.BOOLEAN
+    },
+  }, {
+    sequelize,
+    modelName: 'GroupImage',
+  });
+  return GroupImage;
+};
