1f531b3e9ac5db305e9bcc22407ea79cf58fcc88
Andre <andrehristu@gmail.com>
2023-03-22 17:56:51 -0400

    fixed allownull on eventimage model, add event image based on event id

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -5,12 +5,12 @@ const { Attendance, EventImage, Event, Venue, GroupImage, User, Group, Membershi
 router.get('/', async (req, res, next) => {
     const events = await Event.findAll({
         include:
-        [
-            {model: Venue, attributes: ['id','city','state']},
-            {model: Attendance},
-            {model: EventImage},
-            {model: Group, attributes: ['id','name','city','state']}
-        ]
+            [
+                { model: Venue, attributes: ['id', 'city', 'state'] },
+                { model: Attendance },
+                { model: EventImage },
+                { model: Group, attributes: ['id', 'name', 'city', 'state'] }
+            ]
     });
 
     const eventsList = [];
@@ -19,7 +19,7 @@ router.get('/', async (req, res, next) => {
         eventsList.push(event.toJSON())
     })
 
-    for (let event of eventsList){
+    for (let event of eventsList) {
         delete event.description;
         delete event.createdAt;
         delete event.updatedAt;
@@ -27,15 +27,15 @@ router.get('/', async (req, res, next) => {
         delete event.price;
 
         event.numAttending = 0;
-        for (let attendance of event.Attendances){
-            if (attendance.status === 'attending'){
+        for (let attendance of event.Attendances) {
+            if (attendance.status === 'attending') {
                 event.numAttending++
             }
         }
         delete event.Attendances;
 
-        for (let image of event.EventImages){
-            if (image.preview === true){
+        for (let image of event.EventImages) {
+            if (image.preview === true) {
                 event.previewImage = image.url
             }
         }
@@ -43,22 +43,22 @@ router.get('/', async (req, res, next) => {
 
     }
 
-    return res.json({Events: eventsList});
+    return res.json({ Events: eventsList });
 })
 
 router.get('/:eventId', async (req, res, next) => {
     const event = await Event.findByPk(req.params.eventId, {
         include:
-        [
-            {model: Group, attributes: ['id','name','private','city','state']},
-            {model: Venue, attributes: ['id','address','city','state','lat','lng']},
-            {model: EventImage, attributes: ['id','url','preview']}
-        ],
+            [
+                { model: Group, attributes: ['id', 'name', 'private', 'city', 'state'] },
+                { model: Venue, attributes: ['id', 'address', 'city', 'state', 'lat', 'lng'] },
+                { model: EventImage, attributes: ['id', 'url', 'preview'] }
+            ],
     })
 
-    if (!event){
+    if (!event) {
         res.status(404);
-        res.json({"message": "Event couldn't be found"})
+        res.json({ "message": "Event couldn't be found" })
     }
 
     const resEvent = event.toJSON();
@@ -66,6 +66,55 @@ router.get('/:eventId', async (req, res, next) => {
     delete resEvent.updatedAt;
 
     return res.json(resEvent)
+});
+
+router.post('/:eventId/images', async (req, res, next) => {
+    const { user } = req;
+    const event = await Event.findByPk(req.params.eventId, {
+        include:
+            [
+                { model: Group, include: { model: Membership } },
+                { model: Attendance }
+            ]
+    });
+    if (!user) {
+        res.status(401);
+        return res.json({ "message": "Authentication required" })
+    }
+    if (!event) {
+        res.status(404);
+        res.json({ "message": "Event couldn't be found" })
+    }
+
+    //Authorization
+    for (let membership of event.Group.Memberships) {
+        if (membership.userId === user.id) {
+            if (membership.status !== 'organizer' && membership.status !== 'co-host') {
+                for (let attendance of event.Attendances) {
+                    if (attendance.userId === user.id) {
+                        if (attendance.status !== 'attending') {
+                            res.status(403);
+                            return res.json({ "message": "Forbidden" })
+                        }
+                    }
+                }
+            }
+        }
+    }
+    const {url, preview} = req.body;
+
+    const image = await event.createEventImage({
+        url,
+        preview
+    })
+
+    const resImage = image.toJSON();
+
+    delete resImage.eventId
+    delete resImage.updatedAt
+    delete resImage.createdAt
+
+    res.json(resImage)
 })
 
 module.exports = router;
