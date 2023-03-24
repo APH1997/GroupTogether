c5e6ecf006495929e4d557b34fcf0e0e1f9ff0b8
Andre <andrehristu@gmail.com>
2023-03-24 14:30:26 -0400

    added and connect event images router

---
+++ backend/routes/api/event-images.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const { Attendance, EventImage, Event, Venue, GroupImage, User, Group, Membership, sequelize } = require('../../db/models');
+
+
+module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const groupsRouter = require('./groups.js')
 const venuesRouter = require('./venues.js')
 const eventsRouter = require('./events.js')
 const groupImagesRouter = require('./group-images.js')
+const eventImagesRouter = require('./event-images.js')
 
 const { restoreUser } = require('../../utils/auth.js');
 router.use(restoreUser);
@@ -20,7 +21,9 @@ router.use('/venues', venuesRouter);
 
 router.use('/events', eventsRouter);
 
-router.use('/group-images', groupImagesRouter)
+router.use('/group-images', groupImagesRouter);
+
+router.use('/event-images', eventImagesRouter);
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
