f289dc1e8663fba847490e99e72373ce6463d116
Andre <andrehristu@gmail.com>
2023-04-07 12:44:23 -0400

    login form page component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,16 @@
+import {Route, Switch} from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage/index';
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+      <h1>Hello from App</h1>
+      <Switch>
+        <Route to="/login">
+          <LoginFormPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
