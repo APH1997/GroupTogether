00e4b1f2461a3f5bd57a892320c669b60d8a0e95
Andre <andrehristu@gmail.com>
2023-04-07 13:03:22 -0400

    removed ghost tags from app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,13 +13,11 @@ function App() {
   }, [dispatch]);
   return (
     isLoaded && (
-      <>
         <Switch>
           <Route to="/login">
             <LoginFormPage />
           </Route>
         </Switch>
-      </>
     )
   );
 }
