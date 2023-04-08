541c4820a1b548c5af9f91db2c828fe33ca86458
Andre <andrehristu@gmail.com>
2023-04-08 16:07:26 -0400

    group details component in progress

--- frontend/src/components/Groups/GroupDetails.js
+++ frontend/src/components/Groups/GroupDetails.js
@@ -1,12 +1,12 @@
 import { getGroupDetailsThunk } from "../../store/groups";
-import {useDispatch, useSelector} from "react-redux";
-import {useParams} from "react-router-dom";
-import {useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, NavLink } from "react-router-dom";
+import { useEffect } from "react";
 
-function GroupDetails(){
+function GroupDetails() {
 
     const dispatch = useDispatch()
-    const {groupId} = useParams();
+    const { groupId } = useParams();
 
     const group = useSelector(state => state.groups)[groupId];
 
@@ -15,7 +15,34 @@ function GroupDetails(){
     }, [])
 
     return (
-        <div>{group.id}</div>
+        <>
+            <div className="upperDetails">
+                <div className="upperLeft">
+                    <NavLink to="/groups/all">Back to Groups</NavLink>
+                    <img src=""></img>
+                </div>
+
+                {/* -right side
+        ---Group name
+        ---city, state
+        ---Organize by firstName, lastName
+        ---Join this Group Button */}
+                <div className="upperRight">
+
+                </div>
+
+                {group.id}
+            </div>
+            <div className="lowerDetails">
+                {/*
+            -Organizer
+            -firstName lastName
+            -About (description)
+            -Upcoming Events (#) (if applicable)
+            -Past Events (#) (if applicable)
+            */}
+            </div>
+        </>
     )
 }
 
