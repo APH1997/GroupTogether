fd0284e4d6edab26818ec76c97eaa9d61505d3e2
Andre <andrehristu@gmail.com>
2023-04-11 19:37:20 -0400

    group form renders group info if editing

--- frontend/src/components/Groups/EditGroupForm.js
+++ frontend/src/components/Groups/EditGroupForm.js
@@ -1,8 +1,13 @@
 import GroupForm from "./GroupForm";
+import {useParams} from "react-router-dom"
+import {useSelector} from 'react-redux'
 
 const EditGroupForm = () => {
+    const {groupId} = useParams();
+    const currGroup = useSelector(state => state.groups.allGroups[groupId])
+
     return (
-        <h1>Edit Group Form</h1>
+        <GroupForm type="Update" group={currGroup} />
     )
 }
 export default EditGroupForm
