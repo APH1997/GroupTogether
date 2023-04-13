27932d89de5137df0d5daed42747b89690e12659
Andre <andrehristu@gmail.com>
2023-04-12 22:42:52 -0400

    event form structure

--- frontend/src/components/Events/CreateEventForm.js
+++ frontend/src/components/Events/CreateEventForm.js
@@ -1,22 +1,33 @@
 import EventForm from "./EventForm";
 import {useParams} from "react-router-dom";
+import {useSelector, useDispatch} from 'react-redux';
+import { getGroupDetailsThunk } from "../../store/groups";
+import {useEffect} from 'react';
 
 const CreateEventForm = () => {
-    const groupId = useParams();
-    
+    const dispatch = useDispatch();
+    const {groupId} = useParams();
+    const group = useSelector(state => state.groups.singleGroup)
+
     const event = {
         venueId: "",
+        hostId: group.organizerId || "",
         name: "",
         type: "",
-        capacity: "",
         price: "",
         description: "",
         startDate: "",
-        endDate: "",
+        endDate: ""
     }
 
+    useEffect(() => {
+        dispatch(getGroupDetailsThunk(groupId));
+    }, [dispatch, groupId])
+
+    if (!Object.values(group).length) return <></>
+
     return (
-        <EventForm formType="Create" event={event} groupId={groupId} />
+        <EventForm formType="Create" event={event} group={group} />
     )
 }
 
--- frontend/src/components/Events/EventForm.js
+++ frontend/src/components/Events/EventForm.js
@@ -2,21 +2,112 @@ import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
-function EventForm({formType, event, groupId}) {
+function EventForm({ formType, event, group }) {
     const history = useHistory();
     const dispatch = useDispatch();
-    console.log(groupId);
+    console.log(group);
+
     const sessionUser = useSelector(state => state.session.user)
-    if (!sessionUser){
+
+    if (!sessionUser) {
         history.push('/')
     };
 
-    if (formType === "Update" && sessionUser.id !== event.hostId){
+    if (sessionUser.id !== group.organizerId) {
         history.push('/')
     };
+
+    const [name, setName] = useState(event.name || "");
+    const [type, setType] = useState(event.type || "");
+    const [capacity, setCapacity] = useState(event.capacity || "");
+    const [price, setPrice] = useState(event.price || "");
+    const [description, setDescription] = useState(event.description || "");
+    const [startDate, setStartDate] = useState(event.startDate || "");
+    const [endDate, setEndDate] = useState(event.endDate || "");
+    const [imgUrl, setImgUrl] = useState("")
+
+    console.log(startDate);
 
     return (
-        <h1>Event form</h1>
+        <form>
+
+            <div>
+                <h1>{formType === 'Create' ? `Create an event for ${group.name}` : 'Update your event'}</h1>
+
+                <label htmlFor='nameInput'>What is the name of your event?</label>
+                <input
+                    name="nameInput"
+                    type="text"
+                    placeholder="Event Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div>
+                <div>
+                    <label htmlFor="typeSelect">Is this an in person or online event?</label>
+                    <select name='typeSelect' onChange={e => setType(e.target.value)} value={type}>
+                        <option value="" disabled>(select one)</option>
+                        <option value="In person" >In person</option>
+                        <option value="Online">Online</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="priceInput">What is the price for your event?</label>
+                    <input
+                        type="number"
+                        placeholder="0"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div>
+                <div>
+                    <label htmlFor='startDateInput'>When does your event start?</label>
+                    <input
+                        type="text"
+                        name='startDateInput'
+                        value={startDate}
+                        placeholder='MM/DD/YYYY, HH/mm AM'
+                        onChange={(e) => setStartDate(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='endDateInput'>When does your event end?</label>
+                    <input
+                        type="text"
+                        name='endDateInput'
+                        value={endDate}
+                        placeholder='MM/DD/YYYY, HH/mm PM'
+                        onChange={(e) => setEndDate(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div>
+                <label htmlFor='imageInput'>Please add an image url for your event below</label>
+                <input
+                    type='text'
+                    name='imageInput'
+                    value={imgUrl}
+                    placeholder='Image URL'
+                    onChange={(e) => setImgUrl(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor='describeInput'>Please describe your event:</label>
+                <textarea
+                    name='describeInput'
+                    value={description}
+                    placeholder='Please include at least 30 characters'
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+            </div>
+            <div>
+                <button>{formType === 'Create' ? 'Create' : 'Update'} Event</button>
+            </div>
+
+        </form>
     )
 }
 
