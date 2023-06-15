2e563f9ad4abaca53b99eab0ec98fee373768c25
Andre <andrehristu@gmail.com>
2023-06-15 19:43:35 -0400

    organizer can now remove members

--- frontend/src/components/Groups/ManageGroupModal.js
+++ frontend/src/components/Groups/ManageGroupModal.js
@@ -1,13 +1,20 @@
+import { useDispatch, useSelector } from "react-redux"
+import { deleteMembershipThunk } from "../../store/groups"
+
 function ManageGroup({ group }) {
+    const dispatch = useDispatch()
+    const currGroup = useSelector(state => state.groups.singleGroup)
     function addMember(){
 
     }
-    function removeMember(){
-        
+    function removeMember(id){
+        dispatch(deleteMembershipThunk(group.id, id))
     }
+
     return (
         <div>
             <h1>{group.name} members</h1>
+            {Object.values(group.Memberships).length > 1 ?
             <table id="manage-members-table">
                 <thead>
                     <tr>
@@ -29,12 +36,19 @@ function ManageGroup({ group }) {
                                     {membership.status === "pending" &&
                                         <button>Accept</button>
                                     }
-                                    <button>Remove</button>
+                                    <button onClick={() =>removeMember(membership.User.id)}>Remove</button>
                                 </td>
                             </tr>
                         )}
                 </tbody>
             </table>
+            :
+            <>
+                <h2>Any minute now...</h2>
+                <p>Someone's bound to join your awesome group!</p>
+            </>
+
+            }
         </div>
     )
 }
