65f2d0c446a2863f449e358e1d4026321bfd47c9
Andre <andrehristu@gmail.com>
2023-06-26 13:05:50 -0400

    added users 5 - 24

--- backend/db/seeders/20230317165021-demo-user.js
+++ backend/db/seeders/20230317165021-demo-user.js
@@ -40,6 +40,147 @@ module.exports = {
         username: 'Demo',
         hashedPassword: bcrypt.hashSync('bababooiebanditblimpbobble')
       },
+      // ChatGPT entries
+      {
+        email: 'user1@example.com',
+        firstName: 'John',
+        lastName: 'Doe',
+        username: 'johndoe',
+        hashedPassword: bcrypt.hashSync('password1')
+        },
+        {
+        email: 'user2@example.com',
+        firstName: 'Jane',
+        lastName: 'Smith',
+        username: 'janesmith',
+        hashedPassword: bcrypt.hashSync('password2')
+        },
+        {
+        email: 'user3@example.com',
+        firstName: 'Michael',
+        lastName: 'Johnson',
+        username: 'michaelj',
+        hashedPassword: bcrypt.hashSync('password3')
+        },
+        {
+        email: 'user4@example.com',
+        firstName: 'Emily',
+        lastName: 'Brown',
+        username: 'emilyb',
+        hashedPassword: bcrypt.hashSync('password4')
+        },
+        {
+        email: 'user5@example.com',
+        firstName: 'David',
+        lastName: 'Miller',
+        username: 'davidm',
+        hashedPassword: bcrypt.hashSync('password5')
+        },
+        {
+        email: 'user6@example.com',
+        firstName: 'Sophia',
+        lastName: 'Wilson',
+        username: 'sophiaw',
+        hashedPassword: bcrypt.hashSync('password6')
+        },
+        {
+        email: 'user7@example.com',
+        firstName: 'James',
+        lastName: 'Anderson',
+        username: 'jamesa',
+        hashedPassword: bcrypt.hashSync('password7')
+        },
+        {
+        email: 'user8@example.com',
+        firstName: 'Olivia',
+        lastName: 'Taylor',
+        username: 'oliviat',
+        hashedPassword: bcrypt.hashSync('password8')
+        },
+        {
+        email: 'user9@example.com',
+        firstName: 'Daniel',
+        lastName: 'Clark',
+        username: 'danielc',
+        hashedPassword: bcrypt.hashSync('password9')
+        },
+        {
+        email: 'user10@example.com',
+        firstName: 'Ava',
+        lastName: 'Lee',
+        username: 'avalee',
+        hashedPassword: bcrypt.hashSync('password10')
+        },
+        {
+        email: 'user11@example.com',
+        firstName: 'William',
+        lastName: 'Harris',
+        username: 'williamh',
+        hashedPassword: bcrypt.hashSync('password11')
+        },
+        {
+        email: 'user12@example.com',
+        firstName: 'Isabella',
+        lastName: 'Walker',
+        username: 'isabellaw',
+        hashedPassword: bcrypt.hashSync('password12')
+        },
+        {
+        email: 'user13@example.com',
+        firstName: 'Liam',
+        lastName: 'Turner',
+        username: 'liamt',
+        hashedPassword: bcrypt.hashSync('password13')
+        },
+        {
+        email: 'user14@example.com',
+        firstName: 'Mia',
+        lastName: 'Hill',
+        username: 'miah',
+        hashedPassword: bcrypt.hashSync('password14')
+        },
+        {
+        email: 'user15@example.com',
+        firstName: 'Benjamin',
+        lastName: 'Baker',
+        username: 'benjaminb',
+        hashedPassword: bcrypt.hashSync('password15')
+        },
+        {
+        email: 'user16@example.com',
+        firstName: 'Charlotte',
+        lastName: 'King',
+        username: 'charlottek',
+        hashedPassword: bcrypt.hashSync('password16')
+        },
+        {
+        email: 'user17@example.com',
+        firstName: 'Mason',
+        lastName: 'Green',
+        username: 'masong',
+        hashedPassword: bcrypt.hashSync('password17')
+        },
+        {
+        email: 'user18@example.com',
+        firstName: 'Amelia',
+        lastName: 'Adams',
+        username: 'ameliaa',
+        hashedPassword: bcrypt.hashSync('password18')
+        },
+        {
+        email: 'user19@example.com',
+        firstName: 'Ethan',
+        lastName: 'Campbell',
+        username: 'ethanc',
+        hashedPassword: bcrypt.hashSync('password19')
+        },
+        {
+        email: 'user20@example.com',
+        firstName: 'Harper',
+        lastName: 'Wright',
+        username: 'harperw',
+        hashedPassword: bcrypt.hashSync('password20')
+        }
     ], {});
   },
 
