235668b138a12bbc17b647b7ae6357b0c4046da7
Andre <andrehristu@gmail.com>
2023-06-26 13:49:21 -0400

    added group organizer membership records

--- backend/db/seeders/20230320202243-demo-membership.js
+++ backend/db/seeders/20230320202243-demo-membership.js
@@ -58,6 +58,72 @@ module.exports = {
         groupId: 3,
         status: 'organizer',
       },
+      // chatGPT organizer membership records
+      {
+        userId: 1,
+        groupId: 1,
+        status: 'organizer'
+        },
+        {
+        userId: 2,
+        groupId: 2,
+        status: 'organizer'
+        },
+        {
+        userId: 3,
+        groupId: 3,
+        status: 'organizer'
+        },
+        {
+        userId: 8,
+        groupId: 4,
+        status: 'organizer'
+        },
+        {
+        userId: 16,
+        groupId: 5,
+        status: 'organizer'
+        },
+        {
+        userId: 3,
+        groupId: 6,
+        status: 'organizer'
+        },
+        {
+        userId: 19,
+        groupId: 7,
+        status: 'organizer'
+        },
+        {
+        userId: 12,
+        groupId: 8,
+        status: 'organizer'
+        },
+        {
+        userId: 4,
+        groupId: 9,
+        status: 'organizer'
+        },
+        {
+        userId: 21,
+        groupId: 10,
+        status: 'organizer'
+        },
+        {
+        userId: 6,
+        groupId: 11,
+        status: 'organizer'
+        },
+        {
+        userId: 14,
+        groupId: 12,
+        status: 'organizer'
+        },
+        {
+        userId: 9,
+        groupId: 13,
+        status: 'organizer'
+        }
     ], {})
 
   },
