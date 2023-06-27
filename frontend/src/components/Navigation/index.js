ec3b67fe320c84be2f210642b54a91d54bd8af20
Andre <andrehristu@gmail.com>
2023-06-27 11:34:00 -0400

    added socials

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -4,14 +4,24 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
   return (
     <ul className='nav-bar'>
-      <li>
-        <NavLink exact to="/"><h1>GroupTogether</h1></NavLink>
-      </li>
+      <div className='title-and-socials-container'>
+        <li>
+          <NavLink exact to="/"><h1>GroupTogether</h1></NavLink>
+        </li>
+        <div className='socials'>
+          <a href="https://github.com/APH1997" target="_blank">
+            <i className="fab fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/andre-hristu-012842164/" target="_blank">
+            <i className="fab fa-linkedin"></i>
+          </a>
+        </div>
+      </div>
       <div className={`right-nav${!sessionUser ? ' noUser' : ''}`}>
         {sessionUser && <li><NavLink exact to="/groups/new">Start a new Group</NavLink></li>}
         {isLoaded && (
